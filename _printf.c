0f99c8206e7301b4b175128a2b5aefb48b55e8fc
Earljoe <earljoe06@gmail.com>
2023-09-17 09:00:24 -0700

    _printf %c,%s

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * _printf - a function that produces output according to a format
+ * @format: character string composed of zero or more directives
+ *
+ * Return: the number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+	char *traverse;
+
+	va_start(args, format);
+	for (traverse = (char *)format; *traverse != '\0'; traverse++)
+	{
+	while (*traverse != '%' && *traverse != '\0')
+	{
+		putchar(*traverse);
+		traverse++;
+		count++;
+	}
+	if (*traverse == '\0')
+		break;
+	traverse++; /* skip the '%' */
+	switch (*traverse)
+	{
+		case 'c':
+			putchar(va_arg(args, int));
+			count++;
+			break;
+		case 's':
+			for (char *temp = va_arg(args, char*); *temp != '\0'; temp++)
+			{
+				putchar(*temp);
+				count++;
+			}
+			break;
+		case '%':
+			putchar('%');
+			count++;
+			break;
+		default:
+			break;
+	}
+	}
+	va_end(args);
+	return (count);
+}
