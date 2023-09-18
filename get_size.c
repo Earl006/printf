21965c921457e08d49f1b229a520e42aca9c7d6f
IvyJebet <jebetivy388@gmail.com>
2023-09-18 03:30:23 -0700

    get_size.c File

--- get_size.c
+++ get_size.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * get_size - Calculate size to cast the argument
+ * @format: Formatted string to print the arguments to
+ * @i: List of arguments to be printed
  *
  * Return: Precision.
  */
